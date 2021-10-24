c6756e63ec2c13a0e307ab7591d2f948d20b58da
Aloxan12 <kvakvaaak@mail.ru>
2021-10-24 12:40:23 +0300

    blog style part 1

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $config['title']?></title>
+    <link rel="stylesheet" href="media/css/style.css">
 </head>
 <body>
     <div id="wrapper">
@@ -20,9 +21,9 @@
                     </div>
                     <nav class="header_top_menu">
                         <ul>
-                            <li><a href="#">Главная</a></li>
-                            <li><a href="#">Обо мне</a></li>
-                            <li><a href="#">Я вконтакте</a></li>
+                            <li><a href="/">Главная</a></li>
+                            <li><a href="pages/about_me.php">Обо мне</a></li>
+                            <li><a href="https://vk.com/">Я вконтакте</a></li>
                         </ul>
                     </nav>
                 </div>
